cd738a579b17ec4b80ced903d5df38ec7acb1354
Ben Marley <benjaminjohnmarley@gmail.com>
2021-04-24 13:31:40 +0100

    Style cards and header with styled components

--- src/components/CountriesList.js
+++ src/components/CountriesList.js
@@ -1,22 +1,49 @@
 import React, { Fragment, useContext } from "react";
 import { CountryContext } from "../context/index";
 
+import styled from "styled-components";
+
 const CountryList = () => {
   const appContext = useContext(CountryContext);
   const { loading, countries } = appContext;
   console.log("this is countries", countries);
 
+  const CountryCardContainer = styled.div`
+    background-color: #f2f0eb;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(260px, 1fr));
+    row-gap: 30px;
+    column-gap: 15px;
+  `;
+
+  const CountryCard = styled.div`
+    background-color: #59bb64;
+    color: #fff;
+    padding: 25px 20px;
+  `;
+
+  const CountryCardName = styled.h2`
+    margin: 0 0 10px 0;
+  `;
+
+  const CountryCardNameLocations = styled.h3`
+    margin: 0 0 10px 0;
+  `;
+
   return (
     <Fragment>
-      <h1>List of Country cards</h1>
       {/* {loading && <h1>loading...</h1>} */}
-      {countries.length > 0 &&
-        countries.map((country) => (
-          <div key={country.code}>
-            <h2>{country.name}</h2>
-            <h3>{country.locations}</h3>
-          </div>
-        ))}
+      <CountryCardContainer>
+        {countries.length > 0 &&
+          countries.map((country) => (
+            <CountryCard key={country.code}>
+              <CountryCardName>{country.name}</CountryCardName>
+              <CountryCardNameLocations>
+                Locations: {country.locations}
+              </CountryCardNameLocations>
+            </CountryCard>
+          ))}
+      </CountryCardContainer>
     </Fragment>
   );
 };
